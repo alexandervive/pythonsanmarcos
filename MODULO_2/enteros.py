54f1271a912813217f8e36ab9f766919fbc0430b
alexandervive <164121194+alexandervive@users.noreply.github.com>
2024-03-22 20:30:34 -0500

    Clase 2 - Tipos de datos 1

---
+++ MODULO_2/enteros.py
@@ -0,0 +1,41 @@
+#Son numeros enteros, no tienen decimal
+
+numero_mascotas = 2
+print(type(numero_mascotas))
+
+gastos_mes  = -2000
+print(type(gastos_mes))
+
+zero = 0
+print(type(zero))
+
+dinero_millonario = 111_111_000_000_000
+print("El dinero que tiene el millonario es:", dinero_millonario)
+print("El tipo de dato que almacena la variable dinero_millonario es:", type(dinero_millonario))
+
+#Multiplicacion
+numero_x = 2 * 3
+print(numero_x)
+
+#Potencia
+numero_y = 2 ** 3
+print(numero_y)
+
+a = 10 ** 2
+print (a)   
+
+b = 10 ** 4
+print (b)   
+
+c = 10 ** 4299
+print (c)  
+
+d = 10 ** 4300
+print (d) 
+
+"""a = 10 ** 2
+print (a)""" 
+
+#casteo: Print pasa de un numero a un texto
+#lo que hace internamente es pasar de la clase "int" a la clase "str" (tipo de datos de cadena de texto)
+
---
+++ MODULO_2/variables.py
@@ -0,0 +1,35 @@
+# variable = literal/ constante
+edad = 32
+nombre = "Alexander"
+
+# La funcion print nos ayuda a mostrar la informacion como salida de nuestro programa en la consola o terminal
+print("Mi edad es:", edad)
+print("Mi nombre es:", nombre)
+
+edad = 33
+nombre = "Jhon"
+
+print("Mi edad despues de modificarla es:", edad)
+print("Mi nombre despues de modificarlo es:", nombre)
+
+a = 10
+a = a+1
+print ("El valor de a hora es:", a)
+
+#No podemos asignar nombres a nuestras variabes con palabras reservadas de Python
+#break = 10
+#def = "Hola"
+
+#Objetos
+
+nombre_gino = "Gino"
+print (type(nombre_gino))
+
+numero_estudiantes = 26
+print (type(numero_estudiantes))
+
+# snake case --> Estilo de escritura / nomenclatura en Python para poner nombres a nuestras variables
+dinero_ahorrado = 10000
+
+# camel case
+DineroGuardado = 20000
